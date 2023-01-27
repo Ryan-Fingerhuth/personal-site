1ce514b771fa354d294725a8420144fa2a249c06
Ryan Fingerhuth <rfingerhuth@gmail.com>
2023-01-27 00:27:10 -0500

    Adding and running Prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import Image from 'next/image';
+import { Inter } from '@next/font/google';
+import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -16,8 +16,7 @@ export default function Home() {
           <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
-          >
+            rel="noopener noreferrer">
             By{' '}
             <Image
               src="/vercel.svg"
@@ -50,8 +49,7 @@ export default function Home() {
           href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <h2 className={inter.className}>
             Docs <span>-&gt;</span>
           </h2>
@@ -64,8 +62,7 @@ export default function Home() {
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <h2 className={inter.className}>
             Templates <span>-&gt;</span>
           </h2>
@@ -76,8 +73,7 @@ export default function Home() {
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           <h2 className={inter.className}>
             Deploy <span>-&gt;</span>
           </h2>
@@ -87,5 +83,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
